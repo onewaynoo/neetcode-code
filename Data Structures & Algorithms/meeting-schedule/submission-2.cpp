4479ab5dd6080ae23fe5dd5ef7202db358adfb96
ANKIT KUMAR <ak1357080@gmail.com>
2026-08-29 14:14:20 +0530

    Add: meeting-schedule - submission-2

---
+++ Data Structures & Algorithms/meeting-schedule/submission-2.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition of Interval:
+ * class Interval {
+ * public:
+ *     int start, end;
+ *     Interval(int start, int end) {
+ *         this->start = start;
+ *         this->end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+public:
+    static bool customComparison(const Interval &a, const Interval &b){
+        return a.end<b.end;
+    }
+    bool canAttendMeetings(vector<Interval>& intervals) {
+        if (intervals.empty()) return true;
+        sort(intervals.begin(),intervals.end(),customComparison);
+        for(int i=1;i<intervals.size();i++){
+            if(intervals[i-1].end>intervals[i].start)return false;
+        }
+        return true;
+    }
+};
